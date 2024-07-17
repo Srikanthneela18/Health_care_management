043f5a249a0f4c31ed3627762b069f52ba81b313
Srikanthneela18 <99548811+Srikanthneela18@users.noreply.github.com>
2024-07-17 23:56:11 +0530

    Update DBConnection.java

--- src/main/java/com/dev/healthcare_management/util/DBConnection.java
+++ src/main/java/com/dev/healthcare_management/util/DBConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/hospital";
     private static final String USER = "root";
-    private static final String PASSWORD = "Uppi@007";
+    private static final String PASSWORD = "Gayatri@1472";
 
     public static Connection getConnection() {
         Connection connection = null;
